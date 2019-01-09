eb5c6053b461017f3915954691b1d8c09d673127
ENMANUEL RODRIGUEZ <enmanuel.rodriguez718@gmail.com>
2019-01-09 18:00:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const takeANumber = (katzDeliLine, newCustomerName) => {
       for (let i = 0; i < katzDeliLine.length; ++i) {
         customerOnLineArray.push(`The line is currently: ${i+1}. ${katzDeliLine[i]}`);
       }
-      return 
+      return "The line is currently: "
     } else {
       return "The line is currently empty.";
 
