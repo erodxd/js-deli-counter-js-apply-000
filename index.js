fece40fb8f26347c91f76dd8fb4d0165a3194f1e
ENMANUEL RODRIGUEZ <enmanuel.rodriguez718@gmail.com>
2019-01-09 17:04:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const = () => {
+const takeANumber = (currentLine, newCustomerName) => {
+  let currentNumber = 0;
   
+  for (let i = 0; i < currentLine.length; ++i) {
+    let 
+  }
+  
+  return `Welcome, ${newCustomerName}. You are ${currentNumber} in line.`
 }
