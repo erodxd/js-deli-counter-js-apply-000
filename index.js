9212fc20a89fc6e104c0bd7704687a3e40754189
ENMANUEL RODRIGUEZ <enmanuel.rodriguez718@gmail.com>
2019-01-09 17:26:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const takeANumber = (katzDeliLine, newCustomerName) => {
-  var currentNumber = 0;
+  
   
   for (let i = 0; i < katzDeliLine.length; ++i) {
     var currentElement = katzDeliLine[i];
