ec5d1699ccb480f46c19337f634ee858d7a611d8
ENMANUEL RODRIGUEZ <enmanuel.rodriguez718@gmail.com>
2019-01-09 17:07:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const takeANumber = (currentLine, newCustomerName) => {
   let currentNumber = 0;
   
   for (let i = 0; i < currentLine.length; ++i) {
-    let currentElement = 
+    let currentElement = currentLine[i];
+    
   }
   
   return `Welcome, ${newCustomerName}. You are ${currentNumber} in line.`
