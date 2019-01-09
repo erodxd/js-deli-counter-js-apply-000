a8f4bf68cb658fa00300f0f2899963b0021e3d63
ENMANUEL RODRIGUEZ <enmanuel.rodriguez718@gmail.com>
2019-01-09 17:28:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const takeANumber = (katzDeliLine, newCustomerName) => {
   katzDeliLine.push(newCustomerName);
-  return `Welcome, ${newCustomerName}. You are ${katzDeliLine.length} in line.` 
+  return `Welcome, ${newCustomerName}. You are ${katzDeliLine.length} in line.`;
+}
   
-  for (let i = 0; i < katzDeliLine.length; ++i) {
-    var currentElement = katzDeliLine[i];
-    
-    if (currentElement < currentNumber) {
-      currentNumber++;
-      
-    
-      }
-    }
-  }
   
   const nowServing = (katzDeliLine) => {
     
